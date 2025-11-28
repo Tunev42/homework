313103a66ac49dd622b8605ba699e76437b690b6
Tunev42 <kostik17raven@gmail.com>
2025-11-28 09:54:28 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,7 @@
+# number = [0, 1, 2, 3, 4]
+# result = [x**2 if x % 2 == 0 else -1 for x in number]
+# print(result)
+
+number = [i for i in range(0, 101)]
+result = [x == "Buzz" if x % 3 == 0 else -1 for x in number and]
+print(result)
